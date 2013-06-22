7cc090965dca16316e6b3c2d9a8844c6ff34c21d
LNA <ThisIsLaToyaNicole@gmail.com>
2013-06-22 17:42:13 -0500

    first commit

---
+++ dis/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dis::Application.config.secret_token = 'eb464067a4ff5cc40c1a67d5fe4f902ccf82c94625a0d6884ec0b345717c4714d0d96b3e0424af0eecb8c50b3e73a8d50175352222643a608f40e6d992f76144'
---
+++ red_cross/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedCross::Application.config.secret_key_base = '866e7fe68a7b60c5b22196e3f68277968610a2bf80fbae6d2dfc062027620eb76afd13b3e57079a4fa05a1585f2d864cef1a213571f95b043efae3c398ab8521'
